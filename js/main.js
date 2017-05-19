030af317649482fa0685628b64218b0e9154c24f
Ricardo Mazzini <ricardomzz@gmail.com>
2017-05-19 09:24:30 -0400

    refactor: reimplemented dice as a property of a Player class

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
-var dice=[];
-
-function roll(dice){
-  dice=[];
-for (i = 0; i < 3; i++) {
-    dice.push(Math.floor(Math.random() * 6) + 1);
-
+function Player(){
+  this.dice=[];
+  this.number_of_dice=3;
+  this.roll = function (){
+    for (i=0; i<this.number_of_dice; i++){
+      this.dice.push(Math.floor(Math.random() * 6) + 1);
+    }
+    return this.dice;
+  };
 }
-return dice;
-}
 
-$("#dice").html(roll(dice))
+
+
+
+
+var player_1 = new Player();
+
+$("#dice").html(player_1.roll());
