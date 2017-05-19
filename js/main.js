60257d9739a69295add5b175cd3b340d68280fba
Ricardo Mazzini <ricardomzz@gmail.com>
2017-05-19 09:31:35 -0400

    refactor: moved Player.roll() and Player.lose_die() methods from object instances to object prototype

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 function Player(){
   this.dice=[];
   this.number_of_dice=3;
-  this.roll = function (){
-    for (i=0; i<this.number_of_dice; i++){
-      this.dice.push(Math.floor(Math.random() * 6) + 1);
-    }
-    return this.dice;
-  };
-  this.lose_die = function (){
-    this.number_of_dice = this.number_of_dice-1;
-  };
+
 }
 
+Player.prototype.roll = function (){
+  for (i=0; i<this.number_of_dice; i++){
+    this.dice.push(Math.floor(Math.random() * 6) + 1);
+  }
+  return this.dice;
+};
 
-
+Player.prototype.lose_die = function (){
+  this.number_of_dice = this.number_of_dice-1;
+};
 
 
 var player_1 = new Player();
