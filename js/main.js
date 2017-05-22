574532c39e68f112a6c953232adb89e528cbf6f5
Ricardo Mazzini <ricardomzz@gmail.com>
2017-05-22 12:29:18 -0400

    refactor: Game.turn property now holds a Player instance instead of an integer

--- js/main.js
+++ js/main.js
@@ -22,38 +22,43 @@ Player.prototype.lose_die = function (){
 function Game(){
   this.players=[];
   this.bid={quantity:1,face:1};
-  this.turn=0;
-  this.previous_turn=0;
+  this.turn=null;
+  this.previous_turn=null;
   this.next_turn = function (){
-    this.previous_turn = this.turn;
-    if (this.turn === this.players.length-1) {
-      this.turn = 0;
-    } else {
-      this.turn +=1;
-    }
+    this.previous_turn=this.turn;
+    if (this.turn==this.players[this.players.length-1]){
+      this.turn=this.players[0];
+    } else{this.turn=this.players[this.players.indexOf(this.turn)+1];}
+
   };
   this.render = function(){
     $("#game").html('');
     game.players.forEach(function(player,index,array){
       html='<p>player '+index+'</p>';
       $("#game").append(html);
-      if (game.turn == index) {$("#game").append(
+      if (player===game.turn) {$("#game").append(
         'Dice:  '+player.dice+
         ' Quantity: '+'<input type="number" min="1" id="bid_quantity" step="1" value="'+game.bid.quantity+'" />'+
         ' Face:'+'<input type="number" id="bid_face" min="1" max="6" step="1" value="'+game.bid.face+'" />'+
-        '<button type="button" id="raise">Raise!</button>'+
+        '<button type="button" id="raise">Raise!</button>'
+      );
+      $("#raise").click(function(){
+        quantity=$('#bid_quantity').val();
+        face=$('#bid_face').val();
+        game.raise({quantity:quantity,face:face});
+      });
+      }
+      if (player===game.turn && game.previous_turn) {$("#game").append(
         '<button type="button" id="challenge">Challenge!</button>'
       );
       $("#challenge").click(function(){
         game.challenge();
       });
-      $("#raise").click(function(){
-        quantity=$('#bid_quantity').val();
-        face=$('#bid_face').val();
-        game.raise({quantity:quantity,face:face});
-      })
+      }
 
-      ;}
+
+
+
     });
 
   };
@@ -64,6 +69,7 @@ function Game(){
     this.players.forEach(function(player){
       player.roll();
     });
+    this.turn=this.players[0];
     game.render();
   };
   this.raise = function(bid){
@@ -84,10 +90,10 @@ function Game(){
       });
     });
     if (dice_with_correct_face >= game.bid.quantity){
-      this.players[this.turn].lose_die();
+      this.turn.lose_die();
       alert('player '+this.turn+' lost! and player '+this.previous_turn+'won!count of dice with '+game.bid.face+' face: '+dice_with_correct_face);
     } else {
-      this.players[this.previous_turn].lose_die();
+      this.previous_turn.lose_die();
       alert('player '+this.turn+' won! and player '+this.previous_turn+'lost! count of dice with '+game.bid.face+' face: '+dice_with_correct_face); }
     game.new_round();
   };
@@ -95,7 +101,6 @@ function Game(){
     game.players.forEach(function(player){
       player.roll();
       game.bid={quantity:1,face:1};
-      game.turn=0;
       game.render();
     });
   };
